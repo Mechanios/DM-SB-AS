44647077f98163da9bad65002fe8a8823217244d
Stephen Badger <stephen.badger@gmail.com>
2013-12-07 23:49:39 +0000

    Adds a basic advert class, that will be extended later to provide information on adverts within the client.

---
+++ client-common/src/main/java/com/awesomeware/advertising/client/common/Advert.java
@@ -0,0 +1,10 @@
+package com.awesomeware.advertising.client.common;
+
+/**
+ * An advert, the main deliverable within the client.
+ * 
+ * @author Stephen Badger [stephen.badger@gmail.com]
+ */
+public class Advert {
+
+}
